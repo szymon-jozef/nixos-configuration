59487598d64546c1106cfa3a30f9bb78a630d2b4
Szymon P <szymon_jozef@proton.me>
2026-04-29 17:46:35 +0200

    feat: add new client to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   szymon_pitagoras = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVimjs9wyiBBa0A9UlyhXdQKhI/MrR8CJUudtb28zPf";
   szymon_paderewski = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuD2CO1T4XESxSJ8Cx0aJMUpA8nkZzT83PuzJpW7qr7";
-  szymon_dmowski = "";
+  szymon_dmowski = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGLOp2dwPYyidlEb1iuvG8VlKxhmIrtCzTFfezb9Rm7t";
 
   users = [
     szymon_pitagoras
@@ -11,7 +11,7 @@ let
 
   pitagoras = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdKis81AKMs1Ks457WJVLbzo/WgregoG8wGjnXAJrw+";
   paderewski = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeNmEY42ddkvkuGjEIfxjNvXVhMi29dRhDR0xX6o+6t";
-  dmowski = "";
+  dmowski = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGhnjyiD9lmtENMEj8QKKteZzGetN6LQG2SqSYBeUFR";
   systems = [
     pitagoras
     dmowski
